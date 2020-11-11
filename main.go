9a91298e89737b47c8c9a5879aae3c1741d6772e
jon511 <jonosborne5@me.com>
2020-11-11 19:29:12 +0000

    seperated print control into own file

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"strings"
 	"time"
 )
 
@@ -108,74 +107,6 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 func handleHelp(w http.ResponseWriter, r *http.Request) {
 	pageSetting := PageSettings{Title: "Help", Javascript: "help.js", CSS: "help.css"}
 	tmp.ExecuteTemplate(w, "help.gohtml", pageSetting)
-}
-
-func handlePrintRequest(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	bodyByte, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	bodyString := string(bodyByte)
-	resultsString := strings.Split(bodyString, "&")
-	var results = make(map[string]string)
-	for _, val := range resultsString {
-		element := strings.Split(val, "=")
-		key := element[0]
-		results[key] = element[1]
-	}
-
-	status := "{\"status\":\"Good\"}"
-
-	err = sendToPrinter(results["printCode"], results["ipAddress"])
-	if err != nil {
-		status = "{\"status\":\"Error sending to printer\"}"
-	}
-
-	//status := "{\"status\":\"Good\"}"
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(status))
-
-}
-
-func handleFinalPrintRequest(w http.ResponseWriter, r *http.Request) {
-	pageSetting := PageSettings{Title: "Final Label Print", Javascript: "finalPrint.js", CSS: "finalPrint.css"}
-	tmp.ExecuteTemplate(w, "finalLabel.gohtml", pageSetting)
-}
-
-func handleFinalPrintPost(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	bodyByte, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	bodyString := string(bodyByte)
-	fmt.Println(bodyString)
-
-	retStr := sendSetupTransaction1([]byte(bodyString))
-
-	// resultsString := strings.Split(bodyString, "&")
-	// var results = make(map[string]string)
-	// for _, val := range resultsString {
-	// 	element := strings.Split(val, "=")
-	// 	key := element[0]
-	// 	results[key] = element[1]
-	// }
-
-	// status := "{\"status\":\"Good\"}"
-
-	// err = sendToPrinter(results["printCode"], results["ipAddress"])
-	// if err != nil {
-	// 	status = "{\"status\":\"Error sending to printer\"}"
-	// }
-
-	//status := "{\"status\":\"Good\"}"
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(retStr))
 }
 
 func sendSetupTransaction1(sendData []byte) string {
